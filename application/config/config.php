c6e9ac45812dab85fe64c5bb7edd4bc1119e8518
Trevor N. Suarez <Rican7@gmail.com>
2012-06-12 00:43:01 -0500

    Trying to setup the config file.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://dev.blennd.com/coderwall-app/';
 
 /*
 |--------------------------------------------------------------------------
