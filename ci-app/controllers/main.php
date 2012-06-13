defd911d0b3c841d7f9aded6528039cec14c7385
Trevor N. Suarez <Rican7@gmail.com>
2012-06-13 00:57:10 -0500

    Testing variable assignments from the CI Controllers

--- ci-app/controllers/main.php
+++ ci-app/controllers/main.php
@@ -5,6 +5,7 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
+		$this->smartyparser->assign('variable', 'Smarty!');
 		$this->smartyparser->view('main-index');
 	}
 } // End class Main
